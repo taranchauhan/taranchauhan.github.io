5d250defd574eac7ef9d45cd450a9dc089526cc7
Taran Chauhan <taran.chauhan@hotmail.co.uk>
2019-07-17 09:34:37 +0100

    New Portfolio co commit

---
+++ precache-manifest.23c593db67f19172f9bda391f28456a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5855aeb190784279ca286ec21540b483",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62d6bc42e7571e1dded1",
+    "url": "/static/css/main.5e81deac.chunk.css"
+  },
+  {
+    "revision": "de027f358d173bcc08bd",
+    "url": "/static/js/2.5c81fe46.chunk.js"
+  },
+  {
+    "revision": "62d6bc42e7571e1dded1",
+    "url": "/static/js/main.d909208c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
